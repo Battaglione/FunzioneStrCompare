2b7d18961880d21d02dec2a407a78634dbd082f4
Azel04 <75199595+Azel04@users.noreply.github.com>
2021-02-09 13:17:36 +0100

    Update comparastringa.h

--- comparastringa.h
+++ comparastringa.h
@@ -11,25 +11,25 @@ void comparastringa(char *a, char *b){ //Inizio Funzione tipo void
 	for(i = 0; b[i] != 0; i++) //Verifica lunghezza stringa 2
 		len2++;
 	
-	if(len1 < len2 && len1 != len2){ //Verifica se la prima stringa Ë pi˘ piccola della seconda
+	if(len1 < len2 && len1 != len2){ //Verifica se la prima stringa √® pi√π piccola della seconda
 		printf("Le stringhe sono diverse (La prima e' minore della seconda)\n"); //Se vero: Output seguente frase		
 	}
 	else{ //Se falso
-		if(len1 > len2 && len1 != len2){ //Verifica se la prima stringa Ë pi˘ grande della seconda
+		if(len1 > len2 && len1 != len2){ //Verifica se la prima stringa √® pi√π grande della seconda
 			printf("Le stringhe sono diverse (La prima e' maggiore della seconda)\n"); //Se vero: Output seguente frase		
 		}
 	}
 	if(len1 == len2){ //Verifica se le strighe hanno lunghezza uguale
 		for(i = 0; i < len1; i++){ //Se vero, il programma verifica se le stringhe sono uguali anche nel contenuto
-			if(a[i] == b[i]){ //se il carattere Ë uguale
+			if(a[i] == b[i]){ //se il carattere √® uguale
 				cont++; //incrementa cont
 			}
 		}
-		if(cont == len1){ //Se cont Ë uguale alla lunghezza del vettore
-			printf("Le stringhe sono uguali"); //Output frase seguente
+		if(cont == len1){ //Se cont √® uguale alla lunghezza del vettore
+			printf("Le stringhe sono uguali\n"); //Output frase seguente
 		}
 		else{ //Se falso
-			printf("Le stringhe sono diverse (lunghezza uguale ma contenuto differente)"); //Output frase seguente
+			printf("Le stringhe sono diverse (lunghezza uguale ma contenuto differente)\n"); //Output frase seguente
 		}
 	}
 }
